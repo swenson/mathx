a48f58b9f834710e43c6b9b2023447834676a6c7
Christopher Swenson <chris@caswenson.com>
2014-06-16 21:06:10 -0700

    Update copyrights

--- src/mathx/class_number_test.go
+++ src/mathx/class_number_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2013 Christopher Swenson.
+// Copyright (c) 2013-2014 Christopher Swenson.
 // Copyright (c) 2012 Google, Inc. All Rights Reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
