9a88c046f9a5b911c6091e10eb8f250c4109af00
Georgia Reh <georgia.reh@gmail.com>
2014-10-23 15:38:58 -0700

    preparing for a pull to fix things

--- src/mathx/float/float_test.go
+++ src/mathx/float/float_test.go
@@ -26,20 +26,21 @@ var floatAddTestCases = []struct {
 	exp      int64
 	mantissa *Int
 }{
+	//16 cases, positive negative, between zero and one and greater than one
 	{NewFloat(1.125), NewFloat(40), NewFloat(41.125), true, -3, NewInt(329)},
 	{NewFloat(2), NewFloat(0.0009765625), NewFloat(2.0009765625), true, -10, NewInt(2049)},
 	{NewFloat(0.0087890625), NewFloat(1.75), NewFloat(1.7587890625), true, -10, NewInt(1801)},
 	{NewFloat(0.28125), NewFloat(0.46875), NewFloat(0.75), true, -2, NewInt(3)},
 	{NewFloat(5000), NewFloat(-4099), NewFloat(901), true, 0, NewInt(901)},
 	{NewFloat(2), NewFloat(-0.05859375), NewFloat(1.94140625), true, -8, NewInt(497)},
-	/*{NewFloat(0.00244140625), NewFloat(-20), NewFloat(-19.99755859375), false, -12, NewInt(20477)},
-	/*{NewFloat(0.03125), NewFloat(-0.375), NewFloat(), , ,NewInt()},
-	/*{NewFloat(-1.1), NewFloat(2.4), NewFloat(1.5), true, -1, NewInt(3)},
-	{NewFloat(-), NewFloat(0.0015869140625), NewFloat(), , ,NewInt()},
-	{NewFloat(-0.0234375), NewFloat(), NewFloat(), , ,NewInt()},
-	{NewFloat(-0.0546875), NewFloat(0.34375), NewFloat(), , ,NewInt()},
-	{NewFloat(-), NewFloat(-), NewFloat(), , ,NewInt()},
-	{NewFloat(-), NewFloat(-0.002685546875), NewFloat(), , ,NewInt()},
+	{NewFloat(0.00244140625), NewFloat(-20), NewFloat(-19.99755859375), false, -11, NewInt(-40955)},
+	{NewFloat(0.03125), NewFloat(-0.375), NewFloat(-0.34375), false, -5, NewInt(-11)},
+	/*{NewFloat(-1.1), NewFloat(2.6), NewFloat(1.5), true, -1, NewInt(3)},
+	/*{NewFloat(-), NewFloat(0.0015869140625), NewFloat(), , ,NewInt()},
+	/*{NewFloat(-0.0234375), NewFloat(), NewFloat(), , ,NewInt()},
+	/*{NewFloat(-0.0546875), NewFloat(0.34375), NewFloat(), , ,NewInt()},
+	/*{NewFloat(-), NewFloat(-), NewFloat(), , ,NewInt()},
+	/*{NewFloat(-), NewFloat(-0.002685546875), NewFloat(), , ,NewInt()},
 	{NewFloat(-0.001708984375), NewFloat(-), NewFloat(), , ,NewInt()},
 	{NewFloat(-0.875), NewFloat(-0.25), NewFloat(), , ,NewInt()},
 	{NewFloat(-0.0), NewFloat(1), NewFloat(1), true, 1, NewInt(1)}, this bit fails */
@@ -78,7 +79,67 @@ func TestFloatSub(t *testing.T) {
 	}
 }
 
-var floatStringTestCases = []struct {
+var floatMulTestCases = []struct {
+	a        *Float
+	b        *Float
+	c        *Float //I don't think this variable does anything
+	sign     bool
+	exp      int64
+	mantissa *Int
+}{
+	{NewFloat(2), NewFloat(0.5), NewFloat(1), true, 0, NewInt(1)},
+}
+
+func TestFloatMul(t *testing.T) {
+	x := new(Float)
+	y := new(Float)
+	for _, testCase := range floatMulTestCases {
+		x = testCase.a
+		y = testCase.b
+		z := x.Mul(y)
+		mantissaCmp := testCase.mantissa.Cmp(z.mantissa)
+		if z.sign != testCase.sign || z.exp != testCase.exp || mantissaCmp != 0 {
+			fmt.Printf("%t and %v and %v also %t and %v and %v\n", z.sign, z.exp, z.mantissa, testCase.sign, testCase.exp, testCase.mantissa)
+			t.FailNow()
+		}
+	}
+}
+
+func TestFloatMakeSeventeen(t *testing.T) {
+	z := NewFloat(1.0)
+	z = MakeSeventeen()
+	fmt.Printf("this is z %v\n", z)
+	t.FailNow()
+}
+
+var floatDivTestCases = []struct {
+	a        *Float
+	b        *Float
+	c        *Float //I don't think this variable does anything
+	sign     bool
+	exp      int64
+	mantissa *Int
+}{
+	//{NewFloat(2), NewFloat(2), NewFloat(1), true, 1, NewInt(1)},
+	{NewFloat(20.000000), NewFloat(2.00000000), NewFloat(10), true, 1, NewInt(10)},
+}
+
+func TestFloatDiv(t *testing.T) {
+	x := new(Float)
+	y := new(Float)
+	for _, testCase := range floatDivTestCases { //floatDivTestCases or floatMulTestCAses?
+		x = testCase.a
+		y = testCase.b
+		z := x.Div(y)
+		mantissaCmp := testCase.mantissa.Cmp(z.mantissa)
+		if z.sign != testCase.sign || z.exp != testCase.exp || mantissaCmp != 0 {
+			fmt.Printf("%t and %v and %v also %t and %v and %v\n", z.sign, z.exp, z.mantissa, testCase.sign, testCase.exp, testCase.mantissa)
+			t.FailNow()
+		}
+	}
+}
+
+/*var floatStringTestCases = []struct {
 	num float64
 	str string
 }{
@@ -98,4 +159,4 @@ func TestFloatPrint(t *testing.T) {
 			t.FailNow()
 		}
 	}
-}
+}*/
