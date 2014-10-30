74d0fdd8ce606974e14fd9005e4d2ad4899ac11f
Georgia Reh <georgia.reh@gmail.com>
2014-10-30 12:42:17 -0700

    addition works, probably, but not for zero

--- src/mathx/float/float_test.go
+++ src/mathx/float/float_test.go
@@ -27,14 +27,15 @@ var floatAddTestCases = []struct {
 	mantissa *Int
 }{
 	//16 cases, positive negative, between zero and one and greater than one
+	//Can not use Cmp() for testing because it relies on Sub() and consquently Add(), which would be circular logic
 	{NewFloat(1.125), NewFloat(40), NewFloat(41.125), true, -3, NewInt(329)},
 	{NewFloat(2), NewFloat(0.0009765625), NewFloat(2.0009765625), true, -10, NewInt(2049)},
 	{NewFloat(0.0087890625), NewFloat(1.75), NewFloat(1.7587890625), true, -10, NewInt(1801)},
 	{NewFloat(0.28125), NewFloat(0.46875), NewFloat(0.75), true, -2, NewInt(3)},
 	{NewFloat(5000), NewFloat(-4099), NewFloat(901), true, 0, NewInt(901)},
 	{NewFloat(2), NewFloat(-0.05859375), NewFloat(1.94140625), true, -8, NewInt(497)},
-	{NewFloat(0.00244140625), NewFloat(-20), NewFloat(-19.99755859375), false, -11, NewInt(-40955)},
-	{NewFloat(0.03125), NewFloat(-0.375), NewFloat(-0.34375), false, -5, NewInt(-11)},
+	{NewFloat(0.00244140625), NewFloat(-20), NewFloat(-19.99755859375), false, -11, NewInt(40955)},
+	{NewFloat(0.03125), NewFloat(-0.375), NewFloat(-0.34375), false, -5, NewInt(11)},
 	/*{NewFloat(-1.1), NewFloat(2.6), NewFloat(1.5), true, -1, NewInt(3)},
 	/*{NewFloat(-), NewFloat(0.0015869140625), NewFloat(), , ,NewInt()},
 	/*{NewFloat(-0.0234375), NewFloat(), NewFloat(), , ,NewInt()},
@@ -146,7 +147,7 @@ var floatDivTestCases = []struct {
 
 //don't foreget to add a divide by zero
 
-func TestFloatDiv(t *testing.T) {
+/*func TestFloatDiv(t *testing.T) {
 	precision := NewFloat(2)
 	precision.exp = precision.exp - 50
 	for _, testCase := range floatDivTestCases { //floatDivTestCases or floatMulTestCAses?
@@ -162,7 +163,7 @@ func TestFloatDiv(t *testing.T) {
 			t.FailNow()
 		}
 	}
-}
+} */
 
 /*var floatStringTestCases = []struct {
 	num float64
